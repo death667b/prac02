f8803bf31488c0b04064c9f82615cec3d00593bd
death667b <iw.daniel@student.qut.edu.au>
2016-03-12 15:04:48 +1000

    Almost complete

--- src/footyAnswers/FootyScore.java
+++ src/footyAnswers/FootyScore.java
@@ -9,6 +9,39 @@ package footyAnswers;
  */
 public class FootyScore {
 
+	final int pointsPerGoal = 6;
+	final int pointsPerBehind = 1;
 	
+	private int numberGoals;
+	private int numberBehinds;
+	
+	public FootyScore(){
+		numberGoals = 0;
+		numberBehinds = 0;
+	}
+	
+	public void kickGoal(){
+		numberGoals++;
+	}
 
+	public void kickBehind(){
+		numberBehinds++;
+	}
+	
+	public String sayScore(){
+		return "test";
+	}
+	
+	public int getPoints(){
+		int returnPoints = 0;
+		
+		returnPoints = numberBehinds * pointsPerBehind;
+		returnPoints = returnPoints + (numberGoals * pointsPerGoal);
+		
+		return returnPoints;
+	}
+	
+	public Boolean inFrontOf(){
+		return true;
+	}
 }
