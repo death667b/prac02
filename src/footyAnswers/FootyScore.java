82efef43013457195d26792c557918136d8e1a55
death667b <iw.daniel@student.qut.edu.au>
2016-03-12 15:27:24 +1000

    Added javadoc?

--- src/footyAnswers/FootyScore.java
+++ src/footyAnswers/FootyScore.java
@@ -5,7 +5,7 @@ package footyAnswers;
 
 /**
  * @author Ian Daniel
- *
+ * @version 1.0
  */
 public class FootyScore {
 
@@ -15,19 +15,33 @@ public class FootyScore {
 	private int numberGoals;
 	private int numberBehinds;
 	
+	/**
+	 * Holds details for a AFL football team
+	 */
 	public FootyScore(){
 		numberGoals = 0;
 		numberBehinds = 0;
 	}
 	
+	/**
+	 * Adds one to the number of Goals kicked
+	 */
 	public void kickGoal(){
 		numberGoals++;
 	}
 
+	/**
+	 * Adds on to the number of behind kicked
+	 */
 	public void kickBehind(){
 		numberBehinds++;
 	}
 	
+	/**
+	 * Returns a formatted string of the number of 
+	 * Goals, Behind and total Points
+	 * @return String Team Score
+	 */
 	public String sayScore(){
 		String returnString = "";
 		
@@ -38,6 +52,10 @@ public class FootyScore {
 		return returnString;
 	}
 	
+	/**
+	 * Returns the total points for a team
+	 * @return int Total points for a team
+	 */
 	public int getPoints(){
 		int returnPoints = 0;
 		
@@ -47,6 +65,11 @@ public class FootyScore {
 		return returnPoints;
 	}
 	
+	/**
+	 * Determins who is winning the game
+	 * @param otherPlayer Requires the FootyScore object for the opposing team
+	 * @return Boolean False if other team is winning
+	 */
 	public Boolean inFrontOf(FootyScore otherPlayer){
 		Boolean isWinning = false;
 		
