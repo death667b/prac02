cc09ab96f9946db8946a0b7477fe4ece9d3ed696
Ian Daniel <iw.daniel@student.qut.edu.au>
2016-03-13 13:15:13 +1000

    Add Interface Hero

---
+++ src/heroAnswers/Hero.java
@@ -0,0 +1,12 @@
+package heroAnswers;
+
+/**
+ * Interface hero contains current identity and switch identity
+ * 
+ * @author n5372828 Ian Daniel
+ *
+ */
+public interface Hero { 
+	public String currentIdentity();
+	public void switchIdentity();
+}
